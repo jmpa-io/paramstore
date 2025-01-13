82fc188521243c5e884fd58ba0338ea069866e64
jcleal <mr.jordan.cleal+github@gmail.com>
2025-01-13 18:26:05 +0000

    add tests for get & put

--- get_test.go
+++ get_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/hashicorp/go-multierror"
 )
 
-// mockDeleteParamsFunc is a mock used to mimic the behavior of deleting
+// mockGetParameters is a mock used to mimic the behavior of pulling multiple
 // parameters from AWS SSM Parameter Store.
 func mockGetParameters(
 	t string,
@@ -49,30 +49,6 @@ func mockGetParameters(
 		return out, nil
 	}
 }
-
-//
-// // ssmGetter is a test function used to mock retrieving parameters from AWS SSM Parameter Store.
-// var ssmGetter = func(ctx context.Context, params *ssm.GetParametersInput, optFns ...func(*ssm.Options)) (*ssm.GetParametersOutput, error) {
-// 	if len(params.Names) == 0 {
-// 		return nil, errors.New("no parameters specified")
-// 	}
-// 	testdata := validTestdata.toMap()
-// 	out := &ssm.GetParametersOutput{
-// 		Parameters: make([]types.Parameter, len(params.Names)),
-// 	}
-// 	for i, n := range params.Names {
-// 		param, found := testdata[n]
-// 		if !found {
-// 			return nil, fmt.Errorf("%q is not found in testdata", n)
-// 		}
-// 		out.Parameters[i] = types.Parameter{
-// 			Name:  &param.Name,
-// 			Value: &param.Value,
-// 			Type:  types.ParameterType(param.Type),
-// 		}
-// 	}
-// 	return out, nil
-// }
 
 func Test_Get(t *testing.T) {
 	tests := map[string]struct {
@@ -119,15 +95,6 @@ func Test_Get(t *testing.T) {
 				Errors: []error{validTestdata.toError("failed to get parameter: %v")},
 			},
 		},
-		"catch no parameters given": {
-			client: &Client{
-				logger:    slog.Default(),
-				batchSize: 1,
-				ssmsvc: &mockSSMClient{
-					GetParametersFunc: mockGetParameters("success"),
-				},
-			},
-		},
 	}
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
@@ -165,7 +132,6 @@ func Test_Get(t *testing.T) {
 				t.Errorf("Get() expected a multierror but got a regular error instead;\nwant=%v\ngot=%v\n", tt.errs, err)
 				return
 			}
-
 			if !reflect.DeepEqual(tt.want, got) {
 				t.Errorf(
 					"Get() returned unexpected configuration;\nwant=%+v\ngot=%+v\n",
@@ -185,7 +151,7 @@ func Test_GetMultiple(t *testing.T) {
 		want   Parameters
 		errs   *multierror.Error
 	}{
-		"retrieves parameters successfully": {
+		"get parameters": {
 			client: &Client{
 				withDecryption: true,
 				logger:         slog.Default(),
@@ -198,47 +164,76 @@ func Test_GetMultiple(t *testing.T) {
 			want:  validTestdata.toParameters(),
 			errs:  nil,
 		},
-		// TODO retrieve with decryption set of false.
-		// TODO failures.
+		"catch invalid parameters": {
+			client: &Client{
+				logger:    slog.Default(),
+				batchSize: 1,
+				ssmsvc: &mockSSMClient{
+					GetParametersFunc: mockGetParameters("invalid"),
+				},
+			},
+			names: validTestdata.toSliceString(),
+			errs: &multierror.Error{
+				Errors: validTestdata.toSliceError("%q is an invalid parameter"),
+			},
+		},
+		"catch fail to get parameters": {
+			client: &Client{
+				logger:    slog.Default(),
+				batchSize: 1,
+				ssmsvc: &mockSSMClient{
+					GetParametersFunc: mockGetParameters("error"),
+				},
+			},
+			names: validTestdata.toSliceString(),
+			errs: &multierror.Error{
+				Errors: validTestdata.toSliceError("failed to get parameter: %v"),
+			},
+		},
 	}
-
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
 			got, err := tt.client.GetMultiple(context.Background(), tt.names...)
 
+			// determine what to do if the error returned is a multierror or not.
+			// NOTE: got is populated by the errors.As() function.
 			var errs *multierror.Error
-			if !errors.As(err, &errs) {
-				// expected multi-error.
-			}
+			if errors.As(err, &errs) {
 
-			if errs != tt.errs {
-				t.Errorf(
-					"GetMultiple() returned a number of unexpected errors; want=%v, got=%v",
-					tt.errs,
-					errs,
-				)
-				return
-			}
-			if len(got) != len(tt.want) {
-				t.Errorf(
-					"GetMultiple() returned more or less than expected params; want=%v got=%v",
-					len(tt.want),
-					len(got),
-				)
-				return
-			}
-			for i, p := range got {
-				if p.Name != tt.want[i].Name ||
-					p.Value != tt.want[i].Value ||
-					p.Type != tt.want[i].Type {
+				// check if the number of errors match with the expected errors.
+				if len(errs.Errors) != len(tt.errs.Errors) {
 					t.Errorf(
-						"GetMultiple() returned values that don't match at index %v; want=%+v, got=%+v",
-						i,
-						tt.want[i],
-						p,
+						"GetMultiple() returned unexpected number of errors;\nwant=%v\ngot=%v\n",
+						tt.errs.Errors,
+						errs.Errors,
 					)
 					return
 				}
+
+				// compare each error returned with the expected errors.
+				for i, expectedErr := range tt.errs.Errors {
+					if errs.Errors[i].Error() != expectedErr.Error() {
+						t.Errorf(
+							"GetMultiple() got unexpected error;\nwant=%v\ngot=%v\n",
+							expectedErr,
+							errs.Errors[i].Error(),
+						)
+						return
+					}
+				}
+
+			} else if tt.errs != nil {
+				// an error was returned but it wasn't a multierror.
+				t.Errorf("GetMultiple() expected a multierror but got a regular error instead;\nwant=%v\ngot=%v\n", tt.errs, err)
+				return
+			}
+			if !reflect.DeepEqual(tt.want, got) {
+				t.Errorf(
+					"GetMultiple() returned unexpected configuration;\nwant=%+v\ngot=%+v\n",
+					tt.want,
+					got,
+				)
+				return
 			}
 		})
 	}
--- put_test.go
+++ put_test.go
@@ -11,65 +11,98 @@ import (
 	"github.com/hashicorp/go-multierror"
 )
 
-// ssmPutter is a test function used to mock uploading parameters to AWS SSM Parameter Store.
-var ssmPutter = func(ctx context.Context, input *ssm.PutParameterInput, optFns ...func(*ssm.Options)) (*ssm.PutParameterOutput, error) {
-	if len(*input.Name) == 0 {
-		return nil, errors.New("no parameter specified")
+// mockPutParameter is a mock used to mimic the behavior of uploading a
+// parameter to AWS SSM Parameter Store.
+func mockPutParameter(
+	t string,
+) func(ctx context.Context, input *ssm.PutParameterInput, optFns ...func(*ssm.Options)) (*ssm.PutParameterOutput, error) {
+	return func(ctx context.Context, input *ssm.PutParameterInput, optFns ...func(*ssm.Options)) (*ssm.PutParameterOutput, error) {
+		out := &ssm.PutParameterOutput{}
+		switch t {
+		case "success":
+			// do nothing.
+		case "error":
+			return nil, fmt.Errorf("failed to put parameter: %v", *input.Name)
+		}
+		return out, nil
 	}
-	testdata := validTestdata.toMap()
-	_, found := testdata[*input.Name]
-	if !found {
-		return nil, fmt.Errorf("%q is not found in testdata", *input.Name)
-	}
-	return &ssm.PutParameterOutput{
-		Tier: input.Tier,
-	}, nil
 }
 
 func Test_Put(t *testing.T) {
 	tests := map[string]struct {
-		client *Client
-		params Parameters
-		errs   *multierror.Error
+		client     *Client
+		parameters Parameters
+		errs       *multierror.Error
 	}{
 		"put parameter": {
 			client: &Client{
-				logger: slog.Default(),
+				logger:    slog.Default(),
+				batchSize: 1,
 				ssmsvc: &mockSSMClient{
-					PutParameterFunc: ssmPutter,
+					PutParameterFunc: mockPutParameter("success"),
 				},
 			},
-			params: validTestdata.toParameters(),
-			errs:   nil,
+			parameters: validTestdata.toParameters(),
+			errs:       nil,
 		},
 		"puts parameters": {
 			client: &Client{
-				logger: slog.Default(),
+				logger:    slog.Default(),
+				batchSize: 1,
 				ssmsvc: &mockSSMClient{
-					PutParameterFunc: ssmPutter,
+					PutParameterFunc: mockPutParameter("success"),
 				},
 			},
-			params: validTestdata.toParameters(),
-			errs:   nil,
+			parameters: validTestdata.toParameters(),
+			errs:       nil,
 		},
-		// TODO failures.
-		// TODO test overwrite.
+		"catch fail to put parameters": {
+			client: &Client{
+				logger: slog.Default(),
+				ssmsvc: &mockSSMClient{
+					PutParameterFunc: mockPutParameter("error"),
+				},
+			},
+			parameters: validTestdata.toParameters(),
+			errs: &multierror.Error{
+				Errors: validTestdata.toSliceError("failed to put parameter: %v"),
+			},
+		},
 	}
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
-			err := tt.client.Put(context.Background(), tt.params)
+			err := tt.client.Put(context.Background(), tt.parameters)
 
+			// determine what to do if the error returned is a multierror or not.
+			// NOTE: got is populated by the errors.As() function.
 			var errs *multierror.Error
-			if !errors.As(err, &errs) {
-				// expected multi-error.
-			}
+			if errors.As(err, &errs) {
 
-			if errs != tt.errs {
-				t.Errorf(
-					"Put() returned a number of unexpected errors; want=%v, got=%v",
-					tt.errs,
-					errs,
-				)
+				// check if the number of errors match with the expected errors.
+				if len(errs.Errors) != len(tt.errs.Errors) {
+					t.Errorf(
+						"Put() returned unexpected number of errors;\nwant=%v\ngot=%v\n",
+						len(tt.errs.Errors),
+						len(errs.Errors),
+					)
+					return
+				}
+
+				// compare each error returned with the expected errors.
+				for i, expectedErr := range tt.errs.Errors {
+					if errs.Errors[i].Error() != expectedErr.Error() {
+						t.Errorf(
+							"Put() got unexpected error;\nwant=%v\ngot=%v\n",
+							expectedErr,
+							errs.Errors[i].Error(),
+						)
+						return
+					}
+				}
+
+			} else if tt.errs != nil {
+				// an error was returned but it wasn't a multierror.
+				t.Errorf("Put() expected a multierror but got a regular error instead;\nwant=%v\ngot=%v\n", tt.errs, err)
 				return
 			}
 		})
